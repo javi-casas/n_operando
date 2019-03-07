0328feb5049f9fbe57d56721b0dc96e842495da6
Javi Casas <javier.casas.delgado@gmail.com>
2019-03-07 01:18:16 +0100

    Second change, first commit and push (remote)

--- src/n_operando/Principal.java
+++ src/n_operando/Principal.java
@@ -2,6 +2,6 @@ package n_operando;
 
 public class Principal {
 	public static void main (String[] args) {
-		System.out.println("Vamos a operar en local");
+		System.out.println("Vamos a operar en remoto");
 	}
 }
